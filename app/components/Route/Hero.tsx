28fab12e858cc11e288907b425d4b9b3c263351f
technocrats <technocrats@kiet.edu>
2024-02-15 03:01:35 +0530

    fdsfsdf

--- app/components/Route/Hero.tsx
+++ app/components/Route/Hero.tsx
@@ -1,104 +1,93 @@
-'use client'
-import { useGetHeroDataQuery } from "@/redux/features/layout/layoutApi";
-import Image from "next/image";
+'use client';
 import Link from "next/link";
 import React, { FC, useState } from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 import { BiSearch } from "react-icons/bi";
 import Loader from "../Loader/Loader";
-import { useRouter } from "next/navigation";
+type Props = {
 
-type Props = {};
+}
 
-const Hero: FC<Props> = (props) => {
-  const { data,isLoading } = useGetHeroDataQuery("Banner", {});
+const Hero:FC<Props> = (props) =>{
   const [search,setSearch] = useState("");
   const router = useRouter()
-  
   const handleSearch = () => {
-   if(search === ""){
-    return
-   }else{
-    router.push(`/courses?title=${search}`);
+    if(search === ""){
+     return
+    }else{
+     router.push(`/courses?title=${search}`);
+    }
    }
-  }
 
 
-  return (
-   <>
-   {
-    isLoading ? (
-      <Loader />
-    ) : (
-      <div className="w-full 1000px:flex items-center">
-      <div className="absolute top-[100px] 1000px:top-[unset] 1500px:h-[700px] 1500px:w-[700px] 1100px:h-[600px] 1100px:w-[600px] h-[40vh] left-5 w-[40vh] hero_animation rounded-[50%] 1100px:left-8 1500px:left-14"></div>
-      <div className="1000px:w-[40%] flex 1000px:min-h-screen items-center justify-end pt-[70px] 1000px:pt-[0] z-10">
-        <Image
-          src={data?.layout?.banner?.image.url}
-          width={400}
-          height={400}
-          alt=""
-          className="object-contain 1100px:max-w-[90%] w-[90%] 1500px:max-w-[85%] h-[auto] z-[10]"
-        />
-      </div>
-      <div className="1000px:w-[60%] flex flex-col items-center 1000px:mt-[0px] text-center 1000px:text-left mt-[150px]">
-        <h2 className="dark:text-white text-[#000000c7] text-[30px] px-3 w-full 1000px:text-[70px] font-[600] font-Josefin py-2 1000px:leading-[75px] 1500px:w-[60%] 1100px:w-[78%]">
-          {data?.layout?.banner?.title}
-        </h2>
-        <br />
-        <p className="dark:text-[#edfff4] text-[#000000ac] font-Josefin font-[600] text-[18px] 1500px:!w-[55%] 1100px:!w-[78%]">
-         {data?.layout?.banner?.subTitle}
-        </p>
-        <br />
-        <br />
-        <div className="1500px:w-[55%] 1100px:w-[78%] w-[90%] h-[50px] bg-transparent relative">
-          <input
-            type="search"
-            placeholder="Search Courses..."
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            className="bg-transparent border dark:border-none dark:bg-[#575757] dark:placeholder:text-[#ffffffdd] rounded-[5px] p-2 w-full h-full outline-none text-[#0000004e] dark:text-[#ffffffe6] text-[20px] font-[500] font-Josefin"
-          />
-          <div className="absolute flex items-center justify-center w-[50px] cursor-pointer h-[50px] right-0 top-0 bg-[#39c1f3] rounded-r-[5px]"
+
+return (
+
+
+
+  <> <div>
+     <div className="relative flex flex-col-reverse px-4 py-16 mx-auto lg:block lg:flex-col lg:py-32 xl:py-48 md:px-8 sm:max-w-xl md:max-w-full">
+        <div className="z-0 flex justify-center h-full -mx-4 overflow-hidden lg:pt-24 lg:pb-16 lg:pr-8 xl:pr-0 lg:w-1/2 lg:absolute lg:justify-end lg:bottom-0 lg:left-0 lg:items-center">
+       <Image src={require("../../../public/assests/laptop.png")} alt=""  className= "object-cover object-right w-full h-auto lg:w-auto lg:h-full"/>
+        </div>
+        <div className="relative flex justify-end max-w-xl mx-auto xl:pr-32 lg:max-w-screen-xl">
+          <div className="mb-16 lg:pr-5 lg:max-w-lg lg:mb-0">
+            <div className="max-w-xl mb-6">
+              
+              <h2 className=" max-w-lg mb-8 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none dark:text-white">
+              Secure your ideal 
+                <br className="hidden md:block" />
+               
+                Internship / Job{' '}
+                <span className="inline-block text-deep-purple-accent-400">
+                  online
+                </span>
+              </h2>
+              <p className="text-base text-gray-700 md:text-lg">
+              SpringBee is an experiential platform designed to help you overcome the challenge of having no experience and break the cycle of no job on your resume.
+              </p>
+            </div>
+            <form>
+              <div className="flex  md:flex-row">
+                <input
+                  placeholder="Search Courses ...."
+                  type="search"
+                  value={search}
+                  onChange={(e) => setSearch(e.target.value)}
+                  className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                />
+                <div className=" flex items-center justify-center w-[50px] cursor-pointer h-[48px] ] bg-[#39c1f3] rounded-r-[5px]"
           onClick={handleSearch}
           >
             <BiSearch className="text-white" size={30} />
           </div>
+               
+              </div>
+              <div className="flex items-center mt-4">
+              <button
+                type="submit"
+                className="inline-flex items-center justify-center h-12 px-6 mr-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-purple-950 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+              >
+                Join Now
+              </button>
+                <Link
+                   href="/courses"
+                  
+                  className="dark:text-white inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+                >
+                  View Courses
+                </Link>
+              </div>
+            </form>
+          </div>
         </div>
-        <br />
-        <br />
-        <div className="1500px:w-[55%] 1100px:w-[78%] w-[90%] flex items-center">
-          <Image
-            src={require("../../../public/assests/client-1.jpg")}
-            alt=""
-            className="rounded-full"
-          />
-          <Image
-            src={require("../../../public/assests/client-2.jpg")}
-            alt=""
-            className="rounded-full ml-[-20px]"
-          />
-          <Image
-            src={require("../../../public/assests/client-3.jpg")}
-            alt=""
-            className="rounded-full ml-[-20px]"
-          />
-          <p className="font-Josefin dark:text-[#edfff4] text-[#000000b3] 1000px:pl-3 text-[18px] font-[600]">
-            500K+ People already trusted us.{" "}
-            <Link
-              href="/courses"
-              className="dark:text-[#46e256] text-[crimson]"
-            >
-              View Courses
-            </Link>{" "}
-          </p>
-        </div>
-        <br />
       </div>
-    </div>
-    )
-   }
-   </>
-  );
-};
+    </div></>
+
+   
+)
+
+}
 
 export default Hero;
