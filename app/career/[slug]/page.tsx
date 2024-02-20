edb708ba44f021c5316b0983d0489bd6f4c92a79
technocrats <technocrats@kiet.edu>
2024-02-20 09:32:48 +0530

    some updates

--- app/career/[slug]/page.tsx
+++ app/career/[slug]/page.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Heading from "../../utils/Heading";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import Image from "next/image";
 
 
 type Props = {};
@@ -69,7 +70,7 @@ const Page = (props: Props) => {
           </div>
         </div>
         <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
-          <img
+          <Image
             className="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
             src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
             alt=""
