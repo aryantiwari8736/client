69436a982baf7845a89c4a5d61ceb94bf11fd161
technocrats <technocrats@kiet.edu>
2024-02-21 22:45:50 +0530

    Reviews Updated

--- app/components/Route/Reviews.tsx
+++ app/components/Route/Reviews.tsx
@@ -7,78 +7,78 @@ type Props = {};
 
 export const reviews = [
   {
-    name: "Gene Bates",
-    avatar: "https://randomuser.me/api/portraits/men/1.jpg",
-    profession: "Student | Cambridge university",
+    name: "Sneha Patel",
+    profession: " Entrepreneur",
     comment:
-    "I had the pleasure of exploring Becodemy, a website that provides an extensive range of courses on various tech-related topics. I was thoroughly impressed with my experience, as the website offers a comprehensive selection of courses that cater to different skill levels and interests. If you're looking to enhance your knowledge and skills in the tech industry, I highly recommend checking out Becodemy!",
-},
+      "SpringBee's technical expertise and unwavering support have been instrumental in scaling my online startup. Their personalized approach and commitment to excellence make them an invaluable partner in the journey of entrepreneurship. With SpringBee by my side, I feel confident facing any digital challenge that comes my way",
+  },
   {
-    name: "Verna Santos",
-    avatar: "https://randomuser.me/api/portraits/women/1.jpg",
-    profession: "Full stack developer | Quarter ltd.",
+    name: "Rajesh Sharma",
+    profession: " Software Developer.",
     comment:
-    "Thanks for your amazing programming tutorial channel! Your teaching style is outstanding, and the quality of your tutorials is top-notch. Your ability to break down complex topics into manageable parts, and cover diverse programming languages and topics is truly impressive. The practical applications and real-world examples you incorporate reinforce the theoretical knowledge and provide valuable insights. Your engagement with the audience fosters a supportive learning environment. Thank you for your dedication, expertise, and passion for teaching programming, and keep up the fantastic work!",
-},
+      "As a software developer, I rely on SpringBee for their top-notch technical support and innovative solutions. Their team's depth of knowledge and attention to detail consistently exceed my expectations, enabling me to deliver exceptional results for my clients. SpringBee has become an indispensable part of my toolkit, empowering me to stay ahead in a rapidly evolving digital landscape With SpringBee's comprehensive understanding of emerging technologies, I've been able to tackle complex development challenges with confidence. Their proactive approach to problem-solving and dedication to staying at the forefront of industry trends have helped me streamline my workflows and enhance my productivity.",
+  },
   {
-    name: "Jay Gibbs",
-    avatar: "https://randomuser.me/api/portraits/men/2.jpg",
-    profession: "computer systems engineering student | Zimbabwe",
+    name: "Neha Singh",
+    profession: " Digital Marketer",
     comment:
-    "Thanks for your amazing programming tutorial channel! Your teaching style is outstanding, and the quality of your tutorials is top-notch. Your ability to break down complex topics into manageable parts, and cover diverse programming languages and topics is truly impressive. The practical applications and real-world examples you incorporate reinforce the theoretical knowledge and provide valuable insights. Your engagement with the audience fosters a supportive learning environment. Thank you for your dedication, expertise, and passion for teaching programming, and keep up the fantastic work!"},
+      "SpringBee's consultancy services have been a game-changer for my digital marketing efforts. Their strategic insights and data-driven approach have helped me optimize campaigns and drive meaningful results. I highly recommend their services to fellow marketers. With SpringBee's guidance, I've been able to achieve ROI that exceeds industry standards",
+  },
   {
-    name: "Mina Davidson",
-    avatar: "https://randomuser.me/api/portraits/women/2.jpg",
-    profession: "Junior Web Developer | Indonesia",
+    name: "Amit Patel",
+    profession: " E-commerce Entrepreneu",
     comment:
-    "I had the pleasure of exploring Becodemy, a website that provides an extensive range of courses on various tech-related topics. I was thoroughly impressed with my experience",
-},
+      "SpringBee has been a trusted partner in my e-commerce journey. Their expertise in technical support and digital strategy has been instrumental in navigating the complexities of online retail. Thanks to their guidance, my business has experienced substantial growth.",
+  },
+
   {
-    name: "Rosemary Smith",
-    avatar: "https://randomuser.me/api/portraits/women/3.jpg",
-    profession: "Full stack web developer | Algeria",
+    name: "Priya Gupta",
+    profession: "IT Professional",
     comment:
-    "Your content is very special. The thing I liked the most is that the videos are so long, which means they cover everything in details. for that any person had beginner-level can complete an integrated project when he watches the videos. Thank you very much. Im very excited for the next videos Keep doing this amazing work",
-},
+      "Working with SpringBee has been a refreshing experience. Their team's professionalism and dedication to client success are truly commendable. Whether it's troubleshooting technical issues or implementing new solutions, I can always rely on SpringBee to deliver exceptional results",
+  },
   {
-    name: "Laura Mckenzie",
-    avatar: "https://randomuser.me/api/portraits/women/4.jpg",
-    profession: "Full stack web developer | Canada",
+    name: "Shalni Singh",
+    profession: "IT Professional",
     comment:
-    "Join Becodemy! Becodemy focuses on practical applications rather than just teaching the theory behind programming languages or frameworks. I took a lesson on creating a web marketplace using React JS, and it was very helpful in teaching me the different stages involved in creating a project from start to finish. Overall, I highly recommend Becodemy to anyone looking to improve their programming skills and build practical projects. Becodemy is a great resource that will help you take your skills to the next level.",
-},
+      "Working with SpringBee has been a refreshing experience. Their team's professionalism and dedication to client success are truly commendable. Whether it's troubleshooting technical issues or implementing new solutions, I can always rely on SpringBee to deliver exceptional results",
+  },
 ];
 
 const Reviews = (props: Props) => {
   return (
-  <div className="w-[90%] md:w-[85%] m-auto">
+    <div className="w-[90%] md:w-[85%] m-auto">
       <div className="w-full md:flex items-center">
-      <div className="md:w-[50%] w-full">
-        <Image
-        src={require("../../../public/assests/business-img.png")}
-        alt="business"
-        width={700}
-        height={700}
-        />
+        <div className="md:w-[50%] w-full">
+          <Image
+            src={require("../../../public/assests/business-img.png")}
+            alt="business"
+            width={700}
+            height={700}
+          />
         </div>
         <div className="md:w-[50%] w-full">
           <h3 className={`${styles.title} md:!text-[40px]`}>
-          Discover Our Strength Through  <span className="text-gradient"></span>{" "}
-            <br />Client Testimonials...
+            Discover Our Strength Through{" "}
+            <span className="text-gradient"></span> <br />
+            Client Testimonials...
           </h3>
           <br />
           <p className={styles.label}>
-          Explore firsthand accounts of success and satisfaction from our valued clients. Gain insights into the impact of our services on businesses like yours. Let their experiences guide you in choosing the right partner for your digital journey
+            Explore firsthand accounts of success and satisfaction from our
+            valued clients. Gain insights into the impact of our services on
+            businesses like yours. Let their experiences guide you in choosing
+            the right partner for your digital journey
           </p>
         </div>
         <br />
         <br />
-       </div>
-       <div className="grid grid-cols-1 gap-[25px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-2 lg:gap-[25px] xl:grid-cols-2 xl:gap-[35px] mb-12 border-0 md:[&>*:nth-child(3)]:!mt-[-60px] md:[&>*:nth-child(6)]:!mt-[-20px]">
+      </div>
+      <div className="grid grid-cols-1 gap-[30px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-2 lg:gap-[25px] xl:grid-cols-2 xl:gap-[35px] mb-12 border-0 md:[&>*:nth-child(3)]:!mt-[-60px] md:[&>*:nth-child(6)]:!mt-[-20px]">
         {reviews &&
-            reviews.map((i, index) => <ReviewCard item={i} key={index} />)}
-        </div>
-  </div>
+          reviews.map((i, index) => <ReviewCard item={i} key={index} />)}
+      </div>
+    </div>
   );
 };
 
